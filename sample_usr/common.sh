b0d7f399acffe3cba83d1f9702030a02c4c9ba09
Chihiro Kodama <kodamac@jamstec.go.jp>
2015-07-20 02:45:39 +0900

    add sample_user

---
+++ sample_usr/common.sh
@@ -0,0 +1,16 @@
+DIR_TEMPLATE=/cwork5/kodama/git/template
+
+FILE_LIST_TEMPLATE=( \
+    isccp_matrix.gs  \
+    latlev.gs        \
+    latlon.gs        \
+    latzm.gs         \
+    run_list.gsf     \
+    run_list         \
+    )
+
+OVERWRITE="no"
+#OVERWRITE="yes"   # BE CAREFUL !!!
+
+RESUME="no"
+#RESUME="yes"
